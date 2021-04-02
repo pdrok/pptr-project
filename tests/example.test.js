95e9696d13f8e83d887090ae75f3501c6cff55ae
Pedro Lopez <plopez@poweranalytics.com>
2021-04-02 00:07:12 -0400

    Assertions, Keyboard Press Simulator, wait for Xpath

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 describe('My First Puppeteer Test', () => {
 	it('should launch the browser', async function () {
@@ -8,16 +9,28 @@ describe('My First Puppeteer Test', () => {
 			devtools: false,
 		})
 		const page = await browser.newPage()
-		await page.goto('https://devexpress.github.io/testcafe/example/')
-		await page.type('#developer-name', 'Morty', { delay: 0 })
-		await page.waitForTimeout(2000)
-		await page.click('#tried-test-cafe', { clickCount: 1 })
-		await page.select('#preferred-interface', 'JavaScript API')
-		const message = 'Lets fill that message with some text'
-		await page.type('#comments', message)
-		await page.click('#submit-button')
-		await page.waitForSelector('.result-content')
-		await page.waitForTimeout(2000)
+
+		await page.setDefaultTimeout(10000)
+		await page.setDefaultTimeout(20000)
+
+		await page.goto('http://example.com/')
+		await page.waitForXPath('//h1')
+		const title = await page.title()
+		const url = await page.url()
+		const text = await page.$eval('h1', element => element.textContent)
+		const count = await page.$$eval('p', element => element.length)
+
+		expect(title).to.be.a('string', 'Example Domain')
+		expect(url).to.include('example.com')
+		expect(text).to.be.a('string', 'Example Domain')
+		expect(count).to.equal(2)
+
+		await page.goto('http://zero.webappsecurity.com/index.html')
+		await page.waitForSelector('#searchTerm')
+		await page.type('#searchTerm', 'Hello World')
+		await page.keyboard.press('Enter', { delay: 10 })
+		await page.waitForTimeout(5000)
+
 		await browser.close()
 	})
 })
